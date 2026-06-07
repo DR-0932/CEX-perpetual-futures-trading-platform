7c5a0ece94879b09554a1c24cfd5adaed912bf28
Dhruv <dhruvrajput757@gmail.com>
2026-06-07 09:23:16 +0530

    Update API entry point

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,27 @@
 import express from "express"
-import jwt from 'jsonwebtoken'
-import bcrypt   from 'bcrypt'
-
+import { authRouter } from "./routes/auth-routes.js";
+import { exchangeRouter } from "./routes/exchange-routes.js";
+import { redis } from "@cex/redis";
 
 const app = express();
 app.use(express.json());
 
+app.use("/auth",authRouter);
+app.use("/exchange",exchangeRouter)
 
+async function initRedis(){
+    try{
+        await redis.xgroup("CREATE","orders","engine_group","$","MKSTREAM")
+    }catch(e:any){
+        if(!e.message.include("BUSYGROUP")) throw e
+    }
+}
+
+async function main(){
+    await initRedis();
+    app.listen(3000,()=>{
+        console.log("server running on port 3000")
+    })
+}
+
+main().catch(console.error)
