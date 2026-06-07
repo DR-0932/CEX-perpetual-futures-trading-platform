323c5909696770cbda221c8e07dc21a75869ad72
Dhruv <dhruvrajput757@gmail.com>
2026-06-07 18:34:17 +0530

    listening to redis xreadgroup

--- apps/engine/src/index.ts
+++ apps/engine/src/index.ts
@@ -1,5 +1,47 @@
-/**matching engine gets request from backedn, first read data from queue, keep it running in an
- infinite loop then we take create order 
-*/
 import { redis } from '@cex/redis'
 
+
+async function processMessage(messageId:string,rawFields:string[]){
+    const orderData: Record<string,string> ={}    
+
+    for(let i =0;i<rawFields.length; i +=2){
+        orderData[rawFields[i]]= rawFields[i+1]
+    }
+    
+    console.log(`Processing order [${messageId}]:`,orderData)
+    await redis.xack("orders","engine_group",messageId)
+}
+
+
+async function main() {
+    console.log("engine worker started monitoring orders..")
+    
+    const pending = await redis.xreadgroup(
+        "GROUP",    "engine_group", "worker_1",
+        "COUNT",    "100",
+        "STREAMS",  "orders",       "0"  //read un-ack messages
+    
+    ) as [string, [string, string[]][]][]
+
+    if(pending){
+        for(const[messageId,rawFields] of pending[0][1]){
+            await processMessage(messageId,rawFields)
+        }
+    }
+    while(true) {
+        const result =await redis.xreadgroup(
+            "GROUP",    "engine_group",   "wokrer_1",
+            "COUNT",    "1",
+            "BLOCK",    "0",
+            "STREAMS",  "orders",          ">" 
+        
+        ) as [string,[string,string[]][]][]
+        
+        if(!result) continue;
+        
+        const [messageId,rawfields] = result[0][1][0]
+       await processMessage(messageId,rawfields)
+    }
+}
+
+main().catch(console.error)
