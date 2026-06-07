cb1670c1bc7e579c31ffbc38c9997d914de0d682
Dhruv <dhruvrajput757@gmail.com>
2026-06-07 18:32:21 +0530

    redis and pending messages

--- apps/engine/src/matching-engine.ts
+++ apps/engine/src/matching-engine.ts
@@ -1,14 +1,103 @@
-// 1. get the orderbook for the market
-// 2. if LONG → match against bestAsk, loop while price matches
-// 3. if SHORT → match against bestBid, loop while price matches
-// 4. after matching, if remaining qty → add to orderbook
+import { Order, Orderbook, orderbooks, removeOrder, updateOrderQty,bestAsk,bestBid } from "./orderbook.js";
+import { redis } from "@cex/redis";
 
-//work of matching engine , number 1 swaps calculations, so for that it needs to check orderbook */
+interface fillResult {
+    fill_price:        bigint   
+    fill_qty:          bigint   
+    bid_fully_filled:  boolean
+    ask_fully_filled:  boolean
+    remaining_bid_qty: bigint   
+    remaining_ask_qty: bigint   
+    fill_bid_margin: bigint   
+    fill_ask_margin: bigint 
+}
+
+export  function calculateFill(bestBid:Order,bestAsk:Order) {
+    //fill price will be calculated based on maker,(the one who)
+    // was in the orderbook first, we need timecomparision to decis
+    // who's price we are going to follow*/
+    const fill_price = bestBid.createdAt < bestAsk.createdAt
+    ? bestBid.price
+    : bestAsk.price
+
+    const fill_qty = bestBid.quantity < bestAsk.quantity
+    ? bestBid.quantity
+    : bestAsk.quantity
+
+    const bid_fully_filled = bestBid.quantity<=bestAsk.quantity
+    const ask_fully_filled = bestAsk.quantity<=bestBid.quantity
+
+    const remaining_bid_qty = bestBid.quantity-fill_qty
+    const remaining_ask_qty = bestAsk.quantity-fill_qty
+
+    const fill_bid_margin = (fill_qty*fill_price)/bestBid.leverage
+    const fill_ask_margin = (fill_qty*fill_price)/bestAsk.leverage
+
+    return{
+        fill_price,
+        fill_qty,
+        remaining_ask_qty,
+        remaining_bid_qty,
+        fill_ask_margin,
+        fill_bid_margin,
+        bid_fully_filled,
+        ask_fully_filled
+    }
+}
+
+export async function updateOrderbook(book:Orderbook,bestBid:Order,bestAsk:Order,fill:fillResult){
+    if(fill.bid_fully_filled) {
+        removeOrder(book,bestBid.id)
+    }else{
+        updateOrderQty(book,bestBid.id,fill.remaining_bid_qty)
+    }
+
+    if(fill.ask_fully_filled) {
+        removeOrder(book,bestAsk.id)
+    }else{
+        updateOrderQty(book,bestBid.id,fill.remaining_ask_qty)
+    }
+}
+
+export async function match_orders(market:string):Promise<void> {
+    const book  = orderbooks.get(market)
+    if(!book) return
 
 
-import { Order, Orderbook } from "./orderbook.js";
 
-/** 1 function to swap, will need to check if the order is limit or market,if */
-async function swap(bestBid:Order,bestAsk:Order) {
+    while(true){
+        const best_bid = bestBid(book)
+        const best_ask =bestAsk(book)
 
+        if(!best_ask ||!best_bid) break
+        if(best_bid >best_ask) break
+
+        const fill = calculateFill(best_bid,best_ask)
+        updateOrderbook(book,best_bid,best_ask,fill)
+        
+        await (redis as any).xadd(
+            "fills", "*",
+            "fill_price", String(fill.fill_price),
+            "fill_qty",   String(fill.fill_qty),
+            
+            "market",    market,
+            
+            "bid_orderId",     String(best_bid.id),
+            "ask_orderId",     String(best_ask.id),
+
+            "remaining_bid_qty",    String(fill.remaining_bid_qty),
+            "remaining_ask_qty",     String(fill.remaining_ask_qty),
+
+            "filled_bid_margin",    String(fill.fill_bid_margin),
+            "filled_ask_margin",    String(fill.fill_ask_margin),
+
+            "bid_fully_filled",     String(fill.bid_fully_filled),
+            "ask_fully_filled",     String(fill.ask_fully_filled),
+
+            "bid_leverage",         String(best_bid.leverage),
+            "ask_leverage",         String(best_ask.leverage),
+
+            "timestamp",            String(Date.now())
+        )
+    }
 }
