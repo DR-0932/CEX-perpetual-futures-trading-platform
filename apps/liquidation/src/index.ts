199d5d4670c11fd5a3794b055c77410447cace8f
Dhruv <dhruvrajput757@gmail.com>
2026-06-18 23:59:12 +0530

    liquidation logic update

--- apps/liquidation/src/index.ts
+++ apps/liquidation/src/index.ts
@@ -1,5 +1,22 @@
-// will need to get data from matching engine, 
-/**function to calculate liquidity based on real drop in value from 
- * binance, if it hits 5-10% based on the formula, and trigger a call in matching 
- * engine or BE when it user get liquidated,
- */
+/*  
+    1. get data from matching engine via redis
+
+    2. get data of binance from redis + websocket(no idea how this is going to work)
+
+    3. calculate liquidation price and check if it's matched,(how are we going to keep it checking on perpetual price change, how is
+        can we make it trigger an operation only when there is a fluctuation in price, does binance only send changes in price, or
+        is it insignificant and we can just keep checking with markPrice poller every few ms, is trading that serious or not)
+
+
+*/
+
+import { start_markprice_stream } from "./markprice.js";
+import { redis } from "@cex/redis"
+
+async function main() {
+    console.log("liquidation main function running");
+
+    const pending =await redis.xreadgroup(
+        ""
+    )
+}
