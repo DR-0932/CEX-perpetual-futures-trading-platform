9a2439a652ffd51e4e112f032ebeb3b0db6ff202
Dhruv <dhruvrajput757@gmail.com>
2026-06-29 01:16:19 +0530

    feat(markPricePoller): update mark price calculation and polling

--- apps/markPricePoller/src/index.ts
+++ apps/markPricePoller/src/index.ts
@@ -2,17 +2,19 @@ import WebSocket from "ws";
 import { redis } from "@cex/redis"
 
 function start_markprice_stream(symbol:string){
-    const socketUrl = `wss://fstream.binance.com/ws/${symbol.toLowerCase()}@markPrice`
+    const socketUrl = `wss://stream.binancefuture.com/ws/${symbol.toLowerCase()}@markPrice`
     const ws = new WebSocket(socketUrl)
 
     ws.on('open',()=>{
         console.log(`connected to binance websocket for ${symbol}`)
+         console.log("waiting for messages...")
     });
 
     ws.on('message',async (data)=>{
+        console.log("raw message:", data.toString().slice(0, 100))
         const message = JSON.parse(data.toString())
         const price = BigInt(Math.round(parseFloat(message.p)))
-
+        console.log("price:", price)
         await redis.xadd(
             'markprice',  '*', 
             
