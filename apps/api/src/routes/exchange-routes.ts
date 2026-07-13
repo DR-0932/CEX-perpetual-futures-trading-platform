2628155e17256b50622e627fe8c79be7bfcda96c
Dhruv <dhruvrajput757@gmail.com>
2026-07-13 22:58:01 +0530

    removed faulty logic

--- apps/api/src/routes/exchange-routes.ts
+++ apps/api/src/routes/exchange-routes.ts
@@ -1,4 +1,4 @@
-import { create_orders, cancel_order, on_ramp } from '../controllers/exchange_controllers.js'
+import { create_orders, cancel_order, on_ramp, get_positions, get_collateral, get_orders } from '../controllers/exchange_controllers.js'
 import { authMiddleware } from '../middleware/auth_middleware.js'
 import { Router }    from 'express'
 
@@ -7,3 +7,6 @@ export const exchangeRouter = Router()
 exchangeRouter.post("/create_order", authMiddleware, create_orders)
 exchangeRouter.post("/cancel_order/:id", authMiddleware, cancel_order)
 exchangeRouter.post("/on_ramp", authMiddleware, on_ramp)
+exchangeRouter.get("/positions", authMiddleware, get_positions)
+exchangeRouter.get("/collateral", authMiddleware, get_collateral)
+exchangeRouter.get("/orders",authMiddleware,get_orders)
