1146456cb78fe107491b7a67f74fec21c03797b6
Dhruv <dhruvrajput757@gmail.com>
2026-07-17 23:31:57 +0530

    feat(api): implement JWT authentication

--- apps/api/src/routes/auth-routes.ts
+++ apps/api/src/routes/auth-routes.ts
@@ -1,7 +1,9 @@
 import {Router} from 'express'
-import { signin, signup } from '../controllers/auth_controllers.js';
+import { me, signin, signup } from '../controllers/auth_controllers.js';
+import { authMiddleware } from '../middleware/auth_middleware.js';
 
 export const authRouter = Router()
 
 authRouter.post('/signup',signup);
 authRouter.post('/signin',signin);
+authRouter.get("/me", authMiddleware, me)
