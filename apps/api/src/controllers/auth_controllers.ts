29cd2adbd451a90739d61a3d11e5dfc53a1e156f
Dhruv <dhruvrajput757@gmail.com>
2026-07-26 23:36:10 +0530

    singup

--- apps/api/src/controllers/auth_controllers.ts
+++ apps/api/src/controllers/auth_controllers.ts
@@ -32,43 +32,59 @@ export async function signup(req:Request,res:Response):Promise<void>{
 }
 
 
-export async function signin(req:Request,res:Response):Promise<void>{
-    const {username,password} = req.body;  //add zod
-    if(!username || !password) {
+export async function login(req:Request,res:Response):Promise<void>{
+    const {identifier,password} = req.body;  //add zod
+   
+    if(!identifier || !password) {
         res.status(400).json({error:"missing fields"})
         return
     }    
     
     try{
-        const user = await prisma.user.findUnique({
-            where:{username}
-        })
+        const user = await prisma.user.findFirst({
+                where: {
+                OR: [
+                    { username: identifier },
+                    { email: identifier }
+                ]
+            }
+        });
+
         if(!user){
             res.status(401).json({error:"user not found"})
-            return
+            return;
         }
         
-        const password_match = bcrypt.compare(password,user?.password)
+        const password_match =await bcrypt.compare( password, user?.password )
         if(!password_match){
-            res.status(401).json({error:"invalid password or username"})
+            res.status(401).json({ error:"invalid password or username/email" })
+            return
         }
     
-        const token = jwt.sign({user:user.id},process.env.JWT_SECRET!, {expiresIn:"1d"}) //used ! here,correct it
-        res.status(200).json({token})
+        const token = jwt.sign(
+            { user:user.id },
+            process.env.JWT_SECRET!, //used ! here,need to correct it
+            { expiresIn:"1d" }
+        ) 
+        
+        res.status(200).json({ token })
+    
     }catch(e){
         console.error(e)
-        res.status(500).json({error:"interval server error"})
+        res.status(500).json({ error:"interval server error" })
     }
 }
 
 export async function me(req: Request, res: Response): Promise<void> {
     try {
+        console.log("req.userId:", req.userId);
         const user = await prisma.user.findUnique({
             where: { id: req.userId },
             select: { id: true, username: true, email: true, name: true }
         })
         res.json(user)
+   
     } catch (e) {
-        res.status(500).json({ error: "internal server error" })
+        res.status(500).json({ error: "internal server error",e })
     }
 }
